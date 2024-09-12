27694dd81acb2ff6e3084518acc8742f0ae18531
agustin-Sanchez9 <agustinsanchezp123@gmail.com>
2024-09-12 16:27:48 -0300

    update

--- cgi-bin/p8.c
+++ cgi-bin/p8.c
@@ -1,72 +1,108 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
-#define MAX_CMD_LEN 256
-
-// Función para procesar PATH_INFO y extraer el comando y los argumentos
-void parse_path_info(char *path_info, char *cmd, char *args) {
-    if (path_info == NULL || path_info[0] == '\0') {
-        return;
-    }
-
-    // Saltear la primera barra '/'
-    path_info++;
-
-    // Separar comando y argumentos usando el separador '+'
-    char *token = strsep(&path_info, " ");
-    if (token != NULL) {
-        strncpy(cmd, token, MAX_CMD_LEN - 1);
-    }
-    if (path_info != NULL) {
-        strncpy(args, path_info, MAX_CMD_LEN - 1);
-    }
-}
-
-// Función para ejecutar el comando con argumentos
-void execute_command(const char *cmd, const char *args) {
-    char full_cmd[MAX_CMD_LEN];
-    snprintf(full_cmd, sizeof(full_cmd), "%s %s", cmd, args);
-
-    printf("<pre>");
-    fflush(stdout);  // Necesario para asegurarse de que la salida de printf se vea inmediatamente.
-
-    // Ejecutar el comando
-    FILE *fp = popen(full_cmd, "r");
-    if (fp == NULL) {
-        printf("Error al ejecutar el comando.\n");
-    } else {
-        char buffer[1024];
-        while (fgets(buffer, sizeof(buffer), fp) != NULL) {
-            printf("%s", buffer);
-        }
-        pclose(fp);
-    }
-    printf("</pre>");
-}
+int obtenerQueryString(char *destino, int largo);
+void decodeQueyString(char *str);
 
 int main() {
-    printf("Content-type: text/html\n\n");
-    printf("<html><head><title>Monitoreo del Sistema</title></head><body>\n");
+    char comando[256];
 
-    char cmd[MAX_CMD_LEN] = {0};
-    char args[MAX_CMD_LEN] = {0};
-
-    // Obtener la información de PATH_INFO
-    char *path_info = getenv("PATH_INFO");
-    if (path_info != NULL) {
-        parse_path_info(path_info, cmd, args);
+    if (!obtenerQueryString(comando, sizeof(comando))) {
+        fprintf(stderr, "no fue posible obtener el query string\n");
+        return 1;
     }
 
-    // Validar que se haya pasado un comando
-    if (strlen(cmd) > 0) {
-        printf("<h2>Ejecutando comando: %s %s</h2>\n", cmd, args);
-        execute_command(cmd, args);
+    decodeQueyString(comando);
+
+    printf("Content-Type: text/html\r\n\r\n");
+    printf("<html><body>\n");
+    printf("<p>metodo GET comando %s</p>\n",comando);
+
+    // parseo
+    char *cmd = strtok(comando, " ");
+    char *args[256];
+    int i = 0;
+    while (cmd != NULL && i < 255) {
+        args[i++] = cmd;
+        cmd = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    if (args[0]) {
+        printf("<p>cmd=%s</p>\n", args[0]);
+    }
+    if (i > 1) {
+        printf("<p>args=");
+        for (int k = 1; k < i; k++) {
+            printf("%s", args[k]);
+        }
+        printf("</p>\n");
+    }
+
+    int pipefd[2];
+    if (pipe(pipefd) == -1) {
+        printf("fallo de pipe()");
+        return 1;
+    }
+        
+    pid_t pid;
+    int status;
+    char buffer[1024];
+    ssize_t count;
+
+    pid = fork();
+    if (pid == -1) {
+        printf("fallo de fork()");
+        return 1;
+    }
+
+    if (pid == 0) {
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        execvp(args[0], args);
+        _exit(1);
     } else {
-        printf("<h2>Error: No se ha proporcionado un comando.</h2>\n");
+        close(pipefd[1]);
+        printf("<p></p>"); // para dejar espacio
+        while ((count = read(pipefd[0], buffer, sizeof(buffer) - 1)) > 0) {
+            buffer[count] = '\0'; 
+            printf("<pre>%s</pre>\n", buffer);
+        }
+        close(pipefd[0]);
+        waitpid(pid, &status, 0);
     }
-
     printf("</body></html>\n");
     return 0;
 }
+
+
+int obtenerQueryString(char *destino, int len) {
+    const char *queryString = getenv("QUERY_STRING");
+    if (queryString == NULL)
+        return 0;
+    if (strlen(queryString) >= len)
+        return 0;
+    strcpy(destino, queryString);
+    return 1;
+}
+
+void decodeQueyString(char *string) {
+    char *src = string;
+    char *dst = string;
+    while (*src) {
+        if (src[0] == '%') {
+            int value;
+            sscanf(src + 1, "%2x", &value);
+            *dst++ = (char)value;
+            src += 3;
+        } else {
+            *dst++ = *src++;
+        }
+    }
+    *dst = '\0';
+}
+
